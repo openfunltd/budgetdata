0a02935a84a441c1196f98c0a98a5f05e5baebd1
typingmonk <tmonk.hsieh@tmonk.tw>
2025-04-29 14:33:34 +0800

    Display cross icon when there are currently no data for the unit

--- views/index/index.php
+++ views/index/index.php
@@ -1,3 +1,13 @@
+<?php
+$ret = BudgetAPI::apiQuery(
+    "/proposed_budget_expenditure_by_agencys?limit=0&agg=單位代碼",
+    "透過「歲出機關別預算表」確認目前有哪些單位尚未有資料"
+    );
+$units_with_data = $ret->aggs[0]->buckets;
+$units_with_data = array_map(function($unit) {
+    return $unit->單位代碼;
+}, $units_with_data);
+?>
 <?= $this->partial('common/header', ['title' => $this->app_name]) ?>
 <div class="content-header">
   <h1>預算單位</h1>
@@ -10,6 +20,9 @@
       <?php } ?>
       <div class="col-md-2">
         <a href="/budget/item/unit/<?= $this->escape($unit->機關編號) ?>" class="btn btn-block btn-outline-dark">
+          <?php if (!in_array($unit->機關編號, $units_with_data)) { ?>
+            <i class="bi bi-x-square-fill"></i>
+          <?php } ?>
           <?= $this->escape($unit->機關名稱) ?>
         </a>
       </div>
